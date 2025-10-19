de15b6aed9570e9a944109277ba57c67e915143a
colonelkarani <quavix0000@gmail.com>
2025-10-19 21:58:03 +0300

    farewell baba

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ const express = require("express")
 const app = express()
 const port = 3000
 
-app.set("view engine", "ejs")
-app.use(express.static("views"))
+app.set("view-engine", 'ejs')
 
-app.get("/",(req,res)=>{
-    res.send("home.html")
-})
-app.get("/services",(req,res)=>{
-    res.render("navbar")
+app.get("/user", (req,res)=>{
+    res.render("index.ejs",{name: "Karani"})
 })
 
+app.get("/", (req,res)=>{
+res.render("login.ejs")
+})
 
 app.listen(port, ()=>{
     console.log(`app is listening on http://localhost:${port}`)
