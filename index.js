9a8213df1ee0e184f5d8ad701b136883388d2691
colonelkarani <quavix0000@gmail.com>
2025-10-20 07:38:31 +0300

    mornings

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express")
-
+const bcrypt =require("bcrypt")
+// const { name } = require("ejs")
+const passport = require('passport')
 const app = express()
 const port = 3000
 
+const init = require('./passport_config.js')
+init(passport)
+
+
+let users = []
+
 app.set("view-engine", 'ejs')
+app.use(express.urlencoded({extended: false}))
 
 app.get("/user", (req,res)=>{
     res.render("index.ejs",{name: "Karani"})
@@ -11,6 +20,22 @@ app.get("/user", (req,res)=>{
 
 app.get("/", (req,res)=>{
 res.render("login.ejs")
+})
+
+app.post("/register", async(req,res)=>{
+    try {
+        const hashed = await bcrypt.hash(req.body.signupPassword, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.signupName,
+            email: req.body.signupEmail, 
+            password: hashed
+        })
+        res.redirect("/user", {name: req.body.signupName})
+    } catch (error) {
+        res.redirect("/")
+    }
+    console.log(users)
 })
 
 app.listen(port, ()=>{
